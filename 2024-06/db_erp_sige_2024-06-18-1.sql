00615f4cf160923eb0a965705481d493ded1e49c
Maximiliano Almonte <maximilianoa-te@ZENTYAL-CIBAO.NET>
2024-06-20 09:33:36 -0400

    Actualizacion hasta 2024-06-19

---
+++ 2024-06/db_erp_sige_2024-06-18-1.sql
@@ -0,0 +1,16 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : db_erp_sige_80
+Source Server Version : 50519
+Source Host           : 26.180.237.214:3306
+Source Database       : db_erp_sige
+
+Target Server Type    : MYSQL
+Target Server Version : 50519
+File Encoding         : 65001
+
+Date: 2024-06-18 17:01:24
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
